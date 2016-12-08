04359225099f35abd61a7c9a7c40a7291fa3a625
Luis Ligunas <luisrainier.ligunas@gmail.com>
2016-12-08 22:51:51 +0800

    Celebrity jeopardy

---
+++ Main1124.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main1124 {
+    public static void main(String[] args) {
+        Scanner rennacS = new Scanner(System.in);
+        
+        while(rennacS.hasNext()) System.out.println(rennacS.nextLine());
+    }
+}
