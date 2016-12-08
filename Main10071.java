0465352393d3d3be3a01eda62d641a71e6023f11
Luis Ligunas <luisrainier.ligunas@gmail.com>
2016-12-08 22:58:45 +0800

    Back to High School Physics

---
+++ Main10071.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main10071 {
+    public static void main(String[] args) {
+        Scanner rennacS = new Scanner(System.in);
+        
+        while(rennacS.hasNext()) System.out.println(rennacS.nextLong()*rennacS.nextLong()*2);
+    }
+}
