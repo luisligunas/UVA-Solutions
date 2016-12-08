9284ade32ca55ef040538a9b2bcf40c9495a26ec
Luis Ligunas <luisrainier.ligunas@gmail.com>
2016-12-08 23:55:59 +0800

    Big Chocolate

---
+++ Main10970.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main10970 {
+    public static void main(String[] args) {
+        Scanner rennacS = new Scanner(System.in);
+        
+        while(rennacS.hasNext()) System.out.println(rennacS.nextInt()*rennacS.nextInt() - 1);
+    }
+}
